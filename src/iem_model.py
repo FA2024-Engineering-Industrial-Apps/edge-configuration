5ed888f43f89ff8c8bf7bcfe51fe24acded65ebb
Meleray <konovma@gmail.com>
2024-09-24 16:52:13 +0200

    Add draft of UAConnectorConfig data model

--- src/iem_model.py
+++ src/iem_model.py
@@ -86,6 +86,18 @@ class StringField(ValueField):
 
     def data_type(self) -> str:
         return "string"
+    
+class IntField(ValueField):
+    value: Optional[int]
+
+    def data_type(self) -> int:
+        return "int"
+    
+class BoolField(ValueField):
+    value: Optional[bool]
+
+    def data_type(self) -> bool:
+        return "bool"
 
 
 class IPField(StringField):
@@ -112,7 +124,36 @@ class AbstractAppConfig(ABC, BaseModel):
                     )
                     all_functions += sub_functions
         return all_functions
+    
+    
+# TODO: Create specialized fields, think about which functions are generated for GPT, how updates are handled?    
+    
+class OPCUATagConfig(AbstractAppConfig):
+    name: StringField
+    address: StringField
+    dataType: StringField
+    acquisitionCycle: IntField
+    acquisitionMode: StringField
+    isArrayTypeTag: BoolField
+    accessMode: StringField
+    comments: StringField
+    
+
+class OPCUADatapointConfig(AbstractAppConfig):
+    name: StringField
+    tags: List[OPCUATagConfig]
+    OPCUAUrl: IPField
+    portNumber: IntField
+    # TODO: Create separate field types for fields below cause they are in fact enums
+    authenticationMode: IntField
+    encryptionMode: IntField
+    securityPolicy: IntField
 
 
 class UAConnectorConfig(AbstractAppConfig):
-    ipField: IPField
+    datapoints: List[OPCUADatapointConfig] # For S7, S7Plus change to collection of lists
+    dbservicename: StringField
+    # TODO: Maybe move authentication data somewhere else?
+    username: StringField
+    password: StringField
+    
