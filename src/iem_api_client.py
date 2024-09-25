fc5f5295740175b305077e605cd2ca9910ae671c
Niclas Huelsmann <niclas.huelsmann@tum.de>
2024-09-25 14:22:42 +0200

    cleanup toy examples

--- src/iem_api_client.py
+++ src/iem_api_client.py
@@ -4,7 +4,6 @@ import pydantic
 import os
 from pydantic import BaseModel
 import streamlit as st
-from model import DeviceModel
 
 HOST = "https://ferienakademie-udeliyih.iem.eu1.edge.siemens.cloud"
 LOGIN_ROUTE = HOST + "/portal/api/v1/login/direct"
@@ -31,13 +30,3 @@ def login():
     )
     token = AuthToken.model_validate(response.json()["data"])
     st.session_state["iem_token"] = token.access_token
-
-
-def create_device(device: DeviceModel):
-    ensure_login()
-    response = requests.post(
-        DEVICE_ROUTE,
-        headers={"Authorization": f"Bearer {st.session_state['iem_token']}"},
-        json=device.dict(),
-    )
-    return response.json()
--- src/strategy.py
+++ src/strategy.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from pydantic import BaseModel
 
-from factory import BikePartsFactory, CarPartsFactory, AbstractVehiclePartsFactory
-
-from model import DeviceModel, Vehicle, VehicleTypeEnum
 from llm import retrieve_model
 from iem_model import App, UAConnectorConfig
 
@@ -14,41 +10,6 @@ class Strategy(ABC):
     @abstractmethod
     def send_message(self, prompt: str, history: list) -> str:
         pass
-
-
-class VehicleStrategy(ABC):
-
-    def llm_determine_vehicle_type(self, description: str) -> VehicleTypeEnum:
-        return retrieve_model(
-            description,
-            VehicleTypeEnum,  # type: ignore
-            "You're part of a vehicle factory and returning the configuration parts for a vehicle.",
-        )  # type: ignore
-
-    def get_factory(self, vehicle_type: VehicleTypeEnum) -> AbstractVehiclePartsFactory:
-        if vehicle_type == "bike":
-            return BikePartsFactory()
-        elif vehicle_type == "car":
-            return CarPartsFactory()
-        else:
-            raise ValueError(f"Unknown vehicle type: {vehicle_type}")
-
-    def create_product(self, prompt: str) -> Vehicle:
-        vehicle_type = self.llm_determine_vehicle_type(prompt)
-        factory = self.get_factory(vehicle_type)
-        wheels = factory.create_wheels(prompt)
-        motor = factory.create_motor(prompt)
-        return Vehicle(type=vehicle_type, wheels=wheels, motor=motor)
-
-
-class EdgeDeviceStrategy(Strategy):
-
-    def send_message(self, prompt: str) -> DeviceModel:
-        return retrieve_model(
-            prompt,
-            DeviceModel,  # type: ignore
-            "You're part of a edge device factory and returning the configuration parts for an edge device.",
-        )  # type: ignore
 
 
 class EdgeConfigStrategy(Strategy):
@@ -76,7 +37,7 @@ Ask the user for the values, and answer his questions about the apps and the fie
         opc_ua_connector = App(
             name="OPC UA Connector",
             description="A app which connects to a configured OPC UA Server and collects data from this.",
-            config=UAConnectorConfig()
+            config=UAConnectorConfig(),
         )
         return """
         {{
@@ -84,19 +45,19 @@ Ask the user for the values, and answer his questions about the apps and the fie
                 {0}
             ]
         }}
-        """.format(opc_ua_connector.generate_prompt_string())
+        """.format(
+            opc_ua_connector.generate_prompt_string()
+        )
 
     config_object = UAConnectorConfig()
 
     def send_message(self, prompt: str, history: list) -> str:
         # print(history)
         if not history:
-            adapted_system_prompt = self.system_prompt.format(self.create_app_overview())
+            adapted_system_prompt = self.system_prompt.format(
+                self.create_app_overview()
+            )
             history.append({"role": "system", "content": adapted_system_prompt})
             # print(adapted_system_prompt)
             # print(history)
-        return retrieve_model(
-            prompt,
-            self.config_object,
-            history
-        )
+        return retrieve_model(prompt, self.config_object, history)
