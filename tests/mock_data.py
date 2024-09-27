3085e86fd51aff2a3e1a2d6e6c8cfe9d878c5204
Niclas Huelsmann <niclas.huelsmann@tum.de>
2024-09-27 10:54:15 +0200

    add tests

---
+++ tests/mock_data.py
@@ -0,0 +1,41 @@
+from src.iem_model import AbstractAppConfig, StringField, NestedField, ListField
+
+
+class AuthenticationData(NestedField):
+    username: StringField = StringField(
+        variable_name="username", description="the username of the user", value=None
+    )
+
+    password: StringField = StringField(
+        variable_name="password", description="the users password", value=None
+    )
+
+
+class ContactInformation(NestedField):
+    phone_number: StringField = StringField(
+        variable_name="phone_number",
+        description="the phone number of the user",
+        value=None,
+    )
+
+    address: StringField = StringField(
+        variable_name="address", description="the address of the user", value=None
+    )
+
+
+class ContactList(ListField):
+    blueprint: ContactInformation = ContactInformation(
+        variable_name="Contact_Information",
+        description="The contact information of multiple user",
+    )
+
+
+# Look for default function signatures
+class UserData(AbstractAppConfig):
+    contacts: ContactList = ContactList(variable_name="contacts", description="The contact list")  # type: ignore
+    name: StringField = StringField(
+        variable_name="name", description="The name of the user", value=None
+    )
+
+    def generate_prompt_string(self):
+        return "Needs a username and a string"
