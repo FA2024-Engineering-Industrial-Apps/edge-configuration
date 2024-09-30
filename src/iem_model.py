2b86f792817aef023581bc684b67ced9baa6884f
Meleray <konovma@gmail.com>
2024-09-30 09:45:32 +0200

    Add draft OPC UA Server Model

--- src/iem_model.py
+++ src/iem_model.py
@@ -746,6 +746,38 @@ class DocumentationDatabusConfig(AbstractAppConfig):
         description="Config for live monitoring of communication through MQTT topics."
     )
     
+class OPCUAServerSource(NestedField):
+    name: StringField = StringField(
+        variable_name="name",
+        description="Name of the data source.",
+        value=None
+    )
+    userName: StringField = StringField(
+        variable_name="userName",
+        description="Username used to connect to data source",
+        value=None
+    )
+    password: StringField(
+        variable_name="password",
+        description="Password used to connect to data source",
+        value=None
+    )
+    topic: StringField(
+        variable_name="topic",
+        description="Topic through which the data relevant to the server is transferred",
+        value=None
+    )
+    
+class OPCUAServerConfig(AbstractAppConfig):
+    sourceProviders: ListField = ListField(
+        variable_name="sourceProviders",
+        description="Data sources that send data fields to OPCUA Server",
+        blueprint=OPCUAServerSource(
+            variable_name="source",
+            description="Single data source that sends data fields to server"
+        )
+    )
+
 # For testing TODO
 class DatabusConfig(AbstractAppConfig):
     pass
